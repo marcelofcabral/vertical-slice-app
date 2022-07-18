71fb89b3b44d5053f13afe609cb9f637f14b95cf
MarceloFCabral <marcelofc12@gmail.com>
2022-07-18 15:27:26 -0300

    refactor: changed my mind. Returned AppProvider back to app and deleted app/components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Routes from "./routes";
-import AppProvider from "./components/AppProvider";
+import AppProvider from "./AppProvider";
 
 function App() {
   return (
